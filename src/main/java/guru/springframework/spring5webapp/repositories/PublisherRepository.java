25913d74f86a11d33117fbe72b608e46d51c9230
Jesse Herrera <tuu07965@temple.edu>
2025-06-25 17:54:41 -0400

    Created Publisher Entity and repository. Also edited case for BootStrapData name. Also added Publisher to BootStrapData.

--- src/main/java/guru/springframework/spring5webapp/repositories/PublisherRepository.java
+++ src/main/java/guru/springframework/spring5webapp/repositories/PublisherRepository.java
@@ -1,11 +1,11 @@
 package guru.springframework.spring5webapp.repositories;
 
-import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Publisher;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * Created on 06/25/2025 by Jesse H.
  */
 
-public interface AuthorRepository extends CrudRepository<Author, Long> {
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
 }
