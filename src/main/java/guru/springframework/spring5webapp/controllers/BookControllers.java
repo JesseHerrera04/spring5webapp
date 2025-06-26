68dea907af8750d11d642f13345368f1218fc58b
Jesse Herrera <tuu07965@temple.edu>
2025-06-25 23:42:27 -0400

    Created Book Controller (part 1) and configured Thymeleaf template for view (part 2).

---
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookControllers.java
@@ -0,0 +1,28 @@
+package guru.springframework.spring5webapp.controllers;
+
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created on 06/25/2025 by Jesse H.
+ */
+
+@Controller
+public class BookControllers {
+
+    private final BookRepository bookRepository;
+
+    public BookControllers(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+
+        model.addAttribute("books", bookRepository.findAll());
+
+        return "books/list";
+    }
+}
